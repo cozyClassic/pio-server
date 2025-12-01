bf24a66dd0db9d8dda23cec70e492c9f9143ee35
KIM <46748877+cozyClassic@users.noreply.github.com>
2025-12-01 14:05:58 +0900

    feat: 인터넷 상품 API 수정

--- internet/init_bundle_data.py
+++ internet/init_bundle_data.py
@@ -21,6 +21,33 @@ def parse_integer(value):
         return 0
 
 
+def get_or_create_bundle_promotion(bc_id, coupon_amount, cash_amount):
+    if coupon_amount == 0 and cash_amount == 0:
+        return
+
+    try:
+        bp, created = BundlePromotion.objects.update_or_create(
+            bundle_condition_id=bc_id,
+            defaults={
+                "coupon_amount": coupon_amount,
+                "cash_amount": cash_amount,
+            },
+            create_defaults={
+                "coupon_amount": coupon_amount,
+                "cash_amount": cash_amount,
+            },
+        )
+    except Exception:
+        print(
+            "Error in get_or_create_bundle_promotion:",
+            bc_id,
+            coupon_amount,
+            cash_amount,
+        )
+        print_exc()
+        breakpoint()
+
+
 def get_or_create_bundle_discount(bc_id, text):
     if not text:
         return
@@ -48,7 +75,7 @@ def get_or_create_bundle_discount(bc_id, text):
 
 
 def import_internet_plan_from_excel():
-    wb = load_workbook("internet_plans.xlsx")
+    wb = load_workbook("internet_tv_all.xlsx")
     ws = wb.active
 
     carrier_col = 1  # A
@@ -60,6 +87,8 @@ def import_internet_plan_from_excel():
     etc_discount_col = 14  # N
     install_discount_col = 15  # O
     month_discount_col = 16  # P
+    coupon_col = 17  # Q
+    cash_col = 18  # R
 
     carriers = InternetCarrier.objects.all()
     internet_plans = InternetPlan.objects.select_related("carrier").all()
@@ -74,7 +103,7 @@ def import_internet_plan_from_excel():
 
     print("#######Starting import######")
 
-    for row in ws.iter_rows(min_row=75, max_row=129):  # Skip header row
+    for row in ws.iter_rows(min_row=2, max_row=129):  # Skip header row
         carrier_name = row[carrier_col - 1].value
         internet_plan_id = parse_integer(row[internet_id_col - 1].value)
         wifi_option_id = parse_integer(row[wifi_id_col - 1].value) or None
@@ -128,5 +157,11 @@ def import_internet_plan_from_excel():
         get_or_create_bundle_discount(bc_id, etc_discount)
         get_or_create_bundle_discount(bc_id, install_discount)
         get_or_create_bundle_discount(bc_id, month_discount)
+
+        get_or_create_bundle_promotion(
+            bc_id,
+            parse_integer(row[coupon_col - 1].value),
+            parse_integer(row[cash_col - 1].value),
+        )
 
     return
--- internet/serializers.py
+++ internet/serializers.py
@@ -1,7 +1,7 @@
 from .models import *
 from rest_framework import serializers
+from collections import defaultdict
 from typing import List
-from typing import DefaultDict
 
 
 class InternetCarrierSerializer(serializers.ModelSerializer):
@@ -12,11 +12,39 @@ class InternetCarrierSerializer(serializers.ModelSerializer):
 
 class BCSerializer(serializers.BaseSerializer):
     @staticmethod
-    def get_bundle_discount_amount(bundle_condition: BundleCondition):
-        bundle_discount: List[BundleDiscount] = bundle_condition.bundle_discounts.all()
-        total_month_discount = sum(
-            [b.discount_amount for b in bundle_discount if b.discount_type != "Mobile"]
+    def get_internet_install_fee(
+        bc: BundleCondition,
+        installation_fees: dict[str, int],
+    ):
+        return installation_fees.get("I", 0) - sum(
+            [
+                bd.discount_amount
+                for bd in bc.bundle_discounts.all()
+                if bd.discount_type == "Internet Install"
+            ]
         )
+
+    @staticmethod
+    def get_tv_install_fee(
+        bc: BundleCondition,
+        installation_fees: dict[str, int],
+    ):
+        return installation_fees.get("T", 0) - sum(
+            [
+                bd.discount_amount
+                for bd in bc.bundle_discounts.all()
+                if bd.discount_type == "TV Install"
+            ]
+        )
+
+    @staticmethod
+    def get_bundle_discount(bundle_condition: BundleCondition):
+        bundle_discount: List[BundleDiscount] = bundle_condition.bundle_discounts.all()
+        total_month_discount = {
+            b.discount_type: b.discount_amount
+            for b in bundle_discount
+            if b.discount_type not in ("Mobile", "Internet Install", "TV Install")
+        }
         return total_month_discount
 
     @staticmethod
@@ -28,99 +56,37 @@ class BCSerializer(serializers.BaseSerializer):
         return total_month_discount
 
     @classmethod
-    def get_IT(
+    def get_combine(
         cls,
         bundle_condition: BundleCondition,
-        installation_fee_dict,
+        installation_fees: dict[str, int],
     ):
         if len(bundle_condition.bundle_promotions.all()) == 0:
             return {}
 
         promotion = bundle_condition.bundle_promotions.all()[0]
-        month_discount = cls.get_bundle_discount_amount(bundle_condition)
+        month_discount = cls.get_bundle_discount(bundle_condition)
 
         return {
             "bundle_condition_id": bundle_condition.id,
-            "month_discount": month_discount,
-            "cash": promotion.cash_amount,
-            "coupon": promotion.coupon_amount,
-            "installation_fee": installation_fee_dict[bundle_condition.carrier_id][
-                "IT"
-            ],
-            "mobile_discount": cls.get_mobile_discount(bundle_condition),
-        }
-
-    @classmethod
-    def get_I(
-        cls,
-        bundle_condition: BundleCondition,
-        installation_fee_dict,
-    ):
-        if len(bundle_condition.bundle_promotions.all()) == 0:
-            return {}
-
-        promotion = bundle_condition.bundle_promotions.all()[0]
-        month_discount = cls.get_bundle_discount_amount(bundle_condition)
-
-        return {
-            "bundle_condition_id": bundle_condition.id,
-            "month_discount": month_discount,
-            "cash": promotion.cash_amount,
-            "coupon": promotion.coupon_amount,
-            "installation_fee": installation_fee_dict[bundle_condition.carrier_id]["I"],
-            "mobile_discount": cls.get_mobile_discount(bundle_condition),
-        }
-
-    @classmethod
-    def get_IM(
-        cls,
-        bundle_condition: BundleCondition,
-        installation_fee_dict,
-    ):
-        if len(bundle_condition.bundle_promotions.all()) == 0:
-            return {}
-
-        promotion = bundle_condition.bundle_promotions.all()[0]
-        month_discount = cls.get_bundle_discount_amount(bundle_condition)
-        return {
-            "bundle_condition_id": bundle_condition.id,
-            "month_discount": month_discount,
-            "cash": promotion.cash_amount,
-            "coupon": promotion.coupon_amount,
-            "installation_fee": installation_fee_dict[bundle_condition.carrier_id]["I"],
-            "mobile_discount": cls.get_mobile_discount(bundle_condition),
-        }
-
-    @classmethod
-    def get_IMT(
-        cls,
-        bundle_condition: BundleCondition,
-        installation_fee_dict,
-    ):
-        if len(bundle_condition.bundle_promotions.all()) == 0:
-            return {}
-
-        promotion = bundle_condition.bundle_promotions.all()[0]
-        month_discount = cls.get_bundle_discount_amount(bundle_condition)
-
-        return {
-            "bundle_condition_id": bundle_condition.id,
-            "month_discount": month_discount,
-            "cash": promotion.cash_amount,
-            "coupon": promotion.coupon_amount,
-            "installation_fee": installation_fee_dict[bundle_condition.carrier_id][
-                "IT"
-            ],
-            "mobile_discount": cls.get_mobile_discount(bundle_condition),
+            "discount": {
+                "month": month_discount,
+                "mobile": cls.get_mobile_discount(bundle_condition),
+            },
+            "promotion": {
+                "cash": promotion.cash_amount,
+                "coupon": promotion.coupon_amount,
+            },
+            "installation_fee": {
+                "internet": cls.get_internet_install_fee(
+                    bundle_condition, installation_fees
+                ),
+                "tv": cls.get_tv_install_fee(bundle_condition, installation_fees),
+            },
         }
 
 
 class SimpleInternetPlanSerializer(serializers.ModelSerializer):
-    wifi_fee = serializers.SerializerMethodField()
-
-    def get_wifi_fee(self, obj):
-        return obj.carrier.wifi_router_rental_price_per_month
-
     class Meta:
         model = InternetPlan
         fields = [
@@ -130,18 +96,10 @@ class SimpleInternetPlanSerializer(serializers.ModelSerializer):
             "description",
             "internet_price_per_month",
             "internet_contract_discount",
-            "is_wifi_router_free",
-            "is_wifi_router_selectable",
-            "wifi_fee",
         ]
 
 
 class SimpleTVPlanSerializer(serializers.ModelSerializer):
-    tv_box_fee = serializers.SerializerMethodField()
-
-    def get_tv_box_fee(self, obj):
-        return obj.carrier.tv_settop_box_rental_price_per_month
-
     class Meta:
         model = TVPlan
         fields = [
@@ -151,28 +109,11 @@ class SimpleTVPlanSerializer(serializers.ModelSerializer):
             "description",
             "tv_price_per_month",
             "tv_contract_discount",
-            "is_settop_box_free",
-            "is_settop_box_selectable",
-            "tv_box_fee",
         ]
 
 
 class InternetPlanSerializer(serializers.Serializer):
     def to_representation(self, instance):
-        # 프론트엔드에서 사용할 API 형식?
-        # 1. 인터넷 목록 별도 필요- 기본정보 + 와이파이 렌탈비 여부 및 가격 + 설치비
-        # 2. tv 목록 별도 필요 - 기본정보
-        # 3. 인터넷 목록 + tv = (Internet_ID, TV_ID) 조합별 key 사용
-        # 4. 핸드폰 연결 가능 여부 -> combine을 보고 확인하세요!?
-        # [carrier] : [internet_plan] : [I] / [IT] / [IM] / [IMT]
-        # 4. 각 조건별 가격 정보 + 할인 정보
-        # 5. 기본으로 tv 상품 목록도 나와있어야해서, carrier별 tv_plan도 별도로 불러와야 할 듯
-
-        #  internet_plans: []
-        #  INTERNET_PLAN = { id,  }
-        #  combines: { "I": {}, "IT": {}, "IM": {}, "IMT": {} }
-        #    KEY: "internet_plan_id,tv_plan_id_mobile_type"
-        #    VALUE: { bundle_discount_id, price_per_month, cash, coupon, installation_fee, internet_price, tv_price }
 
         carriers = InternetCarrier.objects.all()
         carrier_dict = {c.id: c.name for c in carriers}
@@ -182,6 +123,8 @@ class InternetPlanSerializer(serializers.Serializer):
                 "logo": carrier.logo.url,
                 "internet_plans": [],
                 "tv_plans": [],
+                "wifi_options": [],
+                "settop_options": [],
                 "combines": {},
             }
             for carrier in carriers
@@ -190,41 +133,56 @@ class InternetPlanSerializer(serializers.Serializer):
             "tv_price_per_month"
         )
 
-        # setup base tv plan list
         for tv_plan in tv_plans:
-            carrier_name = carrier_dict[tv_plan.carrier_id]
-            result[carrier_name]["tv_plans"].append(
+            result[carrier_dict[tv_plan.carrier_id]]["tv_plans"].append(
                 SimpleTVPlanSerializer(tv_plan).data
             )
 
-        # setup base internet plan list
+        wifi_options = WifiOption.objects.all()
+        for wifi_option in wifi_options:
+            result[carrier_dict[wifi_option.carrier_id]]["wifi_options"].append(
+                {
+                    "id": wifi_option.id,
+                    "name": wifi_option.name,
+                    "price_per_month": wifi_option.rental_price_per_month,
+                    "description": wifi_option.description,
+                }
+            )
+
+        settop_options = SettopBoxOption.objects.all()
+        for settop_option in settop_options:
+            result[carrier_dict[settop_option.carrier_id]]["settop_options"].append(
+                {
+                    "id": settop_option.id,
+                    "name": settop_option.name,
+                    "price_per_month": settop_option.rental_price_per_month,
+                    "description": settop_option.description,
+                }
+            )
+
         for internet_plan in instance:
-            result[internet_plan.carrier.name]["internet_plans"].append(
+            result[carrier_dict[internet_plan.carrier_id]]["internet_plans"].append(
                 SimpleInternetPlanSerializer(internet_plan).data
             )
 
         # setup installation dict
-        installation_fee_dict = DefaultDict(dict)
+        installation_fee_dict = defaultdict(dict)
         installation_fees = InstallationOption.objects.all()
         for install_fee in installation_fees:
             installation_fee_dict[install_fee.carrier_id][
                 install_fee.installation_type
             ] = install_fee.installation_fee
 
+        # key_type = internet_id,mobile_type,tv_plan_id,wifi_option_id,settop_box_option_id
+        # I, IW, IT, IM, IWT, IWM, IMT, IWMT
         for plan in instance:
-            #     # 이것도 아니고.. plan 하나에 I, IT, IM, IMT 여러개 다 될 수 있음..
-            #     # -> 하나에 대해서 각각의 serializer를 돌려서 결과를 넣어줘야 함
-            #     # 다만, 해당 serializer에서 조건에 맞는 경우에만 결과를 반환하도록 해야 함
             combines = result[carrier_dict[plan.carrier_id]]["combines"]
             for bc in plan.bundle_conditions.all():
-                key = f"{plan.id},{bc.mobile_type},{bc.tv_plan_id}".replace("None", "")
-                if bc.tv_plan is None and bc.mobile_type == "None":
-                    combines[key] = BCSerializer.get_I(bc, installation_fee_dict)
-                elif bc.tv_plan is not None and bc.mobile_type == "None":
-                    combines[key] = BCSerializer.get_IT(bc, installation_fee_dict)
-                elif bc.tv_plan is None and bc.mobile_type in ["MNO", "MVNO"]:
-                    combines[key] = BCSerializer.get_IM(bc, installation_fee_dict)
-                elif bc.tv_plan is not None and bc.mobile_type in ["MNO", "MVNO"]:
-                    combines[key] = BCSerializer.get_IMT(bc, installation_fee_dict)
+                key = f"{plan.id},{bc.mobile_type},{bc.tv_plan_id},{bc.wifi_option_id},{bc.settop_box_option_id}".replace(
+                    "None", ""
+                )
+                combines[key] = BCSerializer.get_combine(
+                    bc, installation_fee_dict[plan.carrier_id]
+                )
 
         return result
