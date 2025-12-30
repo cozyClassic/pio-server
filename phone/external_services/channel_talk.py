d0a71e5deecaef648164a1f3fc563c0c668f3821
KIM <46748877+cozyClassic@users.noreply.github.com>
2025-12-30 16:45:38 +0900

    refactor: 채널톡 - 공통변수 함수 밖으로 꺼냄

--- phone/external_services/channel_talk.py
+++ phone/external_services/channel_talk.py
@@ -6,12 +6,38 @@ from phoneinone_server.settings import (
 )
 
 
+class ChannelTalkAPI:
+    CHANNELTALK_HEADERS = {
+        "Content-Type": "application/json",
+        "x-access-key": CHANENLTALK_ACCESS_KEY,
+        "x-access-secret": CHANENLTALK_ACCESS_SECRET,
+    }
+    ORDER_ALERT_GROUP_ID = "501418" if DEBUG else "501406"
+
+    @staticmethod
+    def post(cls, url: str, json: dict) -> dict:
+        response = requests.post(
+            url=url,
+            json=json,
+            headers=cls.CHANNELTALK_HEADERS,
+        )
+        return response.json()
+
+    @staticmethod
+    def get(cls, url: str, params: dict = None) -> dict:
+        response = requests.get(
+            url=url,
+            params=params,
+            headers=cls.CHANNELTALK_HEADERS,
+        )
+        return response.json()
+
+
 def send_order_alert(order_id: str, customer_name: str, customer_phone: str) -> None:
     """Send a message to the team via Channel Talk."""
     # Implementation for sending message via Channel Talk
-    ORDER_ALERT_GROUP_ID = "501418" if DEBUG else "501406"
-    response = requests.post(
-        url=f"https://api.channel.io/open/v5/groups/{ORDER_ALERT_GROUP_ID}/messages",
+    response = ChannelTalkAPI.post(
+        url=f"https://api.channel.io/open/v5/groups/{ChannelTalkAPI.ORDER_ALERT_GROUP_ID}/messages",
         json={
             "blocks": [
                 {
@@ -19,11 +45,6 @@ def send_order_alert(order_id: str, customer_name: str, customer_phone: str) -> 
                     "value": f"주문 알림: {order_id}, {customer_name}, {customer_phone}",
                 },
             ]
-        },
-        headers={
-            "Content-Type": "application/json",
-            "x-access-key": CHANENLTALK_ACCESS_KEY,
-            "x-access-secret": CHANENLTALK_ACCESS_SECRET,
         },
     )
     return response.json()
