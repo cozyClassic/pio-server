bfd052591cc324347054ec3a9f77377d3be3ba91
KIM <46748877+cozyClassic@users.noreply.github.com>
2025-12-31 17:15:12 +0900

    chore: CarrierChoices에서 value를 SKT -> SK, LG U+ -> LG, MVNO -> 알뜰폰으로 수정

--- phone/constants.py
+++ phone/constants.py
@@ -2,11 +2,11 @@ class CarrierChoices(object):
     SK = "SK"
     KT = "KT"
     LG = "LG"
-    MVNO = "MVNO"
+    MVNO = "알뜰폰"
     CHOICES = [
-        (SK, "SK Telecom"),
-        (KT, "KT Corporation"),
-        (LG, "LG Uplus"),
-        (MVNO, "MVNO"),
+        (SK, "SK"),
+        (KT, "KT"),
+        (LG, "LG"),
+        (MVNO, "알뜰폰"),
     ]
     VALUES = [SK, KT, LG, MVNO]
